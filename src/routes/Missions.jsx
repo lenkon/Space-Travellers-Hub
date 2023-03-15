64cbe5c1153df1e515ff4a2aaa5fd9c2a3fe5996
Ayo Moses <ayomoses111@gmail.com>
2023-03-15 22:14:00 +0100

    fixed styling

--- src/routes/Missions.jsx
+++ src/routes/Missions.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeStatus } from '../redux/missions/missionSlice';
 
@@ -17,13 +19,29 @@ function Missions() {
             <th>g</th>
           </tr>
         </thead>
+
+        {/*  bg={variant.toLowerCase()} */}
         <tbody>
           {missions.map((mission) => (
             <tr key={mission.id}>
               <td>{mission.name}</td>
               <td>{mission.description}</td>
-              <td>{!mission.reserved ? 'NOT A MEMBER' : 'Active Member'}</td>
-              <td><div className="center-align"><button type="button" onClick={() => dispatch(changeStatus(mission.id))}>{!mission.reserved ? 'JOIN MISSION' : 'Leave Mission'}</button></div></td>
+              <td>
+                <Badge bg={!mission.reserved ? 'secondary' : 'success'}>
+                  {!mission.reserved ? 'NOT A MEMBER' : 'Active Member'}
+                </Badge>
+              </td>
+              <td>
+                <div className="center-align">
+                  <Button
+                    type="button"
+                    variant={!mission.reserved ? 'outline-success' : 'outline-danger'}
+                    onClick={() => dispatch(changeStatus(mission.id))}
+                  >
+                    {!mission.reserved ? 'JOIN MISSION' : 'Leave Mission'}
+                  </Button>
+                </div>
+              </td>
             </tr>
           ))}
         </tbody>
--- src/routes/MyProfile.jsx
+++ src/routes/MyProfile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { useSelector } from 'react-redux';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const MyProfile = () => {
   const { missions } = useSelector((state) => state.missionsReducer);
@@ -10,9 +11,14 @@ const MyProfile = () => {
 
   return (
     <Container>
-      <Row>
+      <Row className="mr-y">
         <Col>
-          {filteredMissions.map((data) => (<div key={data.id}>{data.name}</div>))}
+          <h3>My Missions</h3>
+          <ListGroup>
+            {filteredMissions.map((data) => (
+              <ListGroup.Item key={data.id}>{data.name}</ListGroup.Item>
+            ))}
+          </ListGroup>
         </Col>
         <Col />
       </Row>
