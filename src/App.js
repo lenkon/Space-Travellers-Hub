705a8b62866c073092bd2d90c9dadcb863cd07a3
lenkon <inoulenkon@gmail.com>
2023-03-15 10:11:39 +0530

    Create website header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import MyProfile from './components/MyProfile';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        App Component
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/myProfile" element={<MyProfile />} />
+      </Routes>
+    </BrowserRouter>
+  </>
+);
 
 export default App;
