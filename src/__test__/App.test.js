a5d9bc829b7be8d35d74e31b135cecff442a8bf2
Ayo Moses <ayomoses111@gmail.com>
2023-03-17 16:37:24 +0100

    fixed linter errors

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
