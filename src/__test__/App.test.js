9949d85c2240e0ee45acf580df863020eadce5d7
Ayo Moses <ayomoses111@gmail.com>
2023-03-17 16:34:31 +0100

    initialized tests for each component

---
+++ src/__test__/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+import App from '../App';
+
+it('App component test', () => {
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
