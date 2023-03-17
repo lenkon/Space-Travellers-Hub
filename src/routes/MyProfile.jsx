bd18f9c00a760540940d9def9de35670f9b2e5be
lenkon <inoulenkon@gmail.com>
2023-03-17 10:55:34 +0530

    Display reserved rockets in My profile

--- src/routes/MyProfile.jsx
+++ src/routes/MyProfile.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
@@ -8,6 +9,7 @@ import ListGroup from 'react-bootstrap/ListGroup';
 const MyProfile = () => {
   const { missions } = useSelector((state) => state.missionsReducer);
   const filteredMissions = missions.filter((mission) => mission.reserved);
+  const { rockets } = useSelector((state) => state.rockets);
 
   return (
     <Container>
@@ -20,7 +22,29 @@ const MyProfile = () => {
             ))}
           </ListGroup>
         </Col>
-        <Col />
+
+        <Col>
+          <Card border="white">
+            <Card.Body>
+              <h3>My Rockets</h3>
+              <Card.Text>
+                <ul className="list-group">
+                  {rockets.map(({
+                    id,
+                    name,
+                    reserved,
+                  }) => (
+                    reserved && (
+                    <li key={id} className="list-group-item">
+                      {name}
+                    </li>
+                    )
+                  ))}
+                </ul>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
       </Row>
     </Container>
   );
